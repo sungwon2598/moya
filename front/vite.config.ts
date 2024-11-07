b0b9ef37de13f7d3be63a987eab42b2018b71d66
pillow12360 <ehdcks1256@naver.com>
2024-11-07 23:23:51 +0900

    KAN-80 feat: eslint, prettier, alias, ts, 환경변수 초기세팅

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';  // node: prefix 사용
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, './src') },
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+      { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+      { find: '@hooks', replacement: path.resolve(__dirname, './src/hooks') },
+      { find: '@utils', replacement: path.resolve(__dirname, './src/utils') },
+      { find: '@types', replacement: path.resolve(__dirname, './src/types') },
+      { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') }
+    ]
+  }
+});
