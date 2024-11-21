95a693c50b836e24a6a71961bd06195e583c3064
pillow12360 <ehdcks1256@naver.com>
2024-11-21 15:01:05 +0900

    KAN-103 refactor: 기존 페이지 라우트 방식으로 리펙토링

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
-import { router } from './routes';
-import {ModalProvider} from "./context/ModalContext.tsx";
+import { ModalProvider } from './context/ModalContext';
+import RootLayout from './layout/RootLayout';
+import Main from './pages/main/Main';
+import RoadmapPreview from './pages/RoadmapPreview';
+// import LearningRoad from './pages/learningRoad/LearningRoad';
+import ChatList from './pages/chat/ChatList';
+import ChatRoom from './pages/chat/ChatRoom';
+
 
 const App: React.FC = () => {
     return (
         <AuthProvider>
             <ModalProvider>
-            <RouterProvider router={router} />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<RootLayout />}>
+                            <Route index element={<Main />} />
+                            <Route path="roadmap-preview" element={<RoadmapPreview />} />
+                            <Route path="preview">
+                                {/* <Route index element={<LearningRoad />} /> */}
+                            </Route>
+                            {/* 향후 추가될 라우트들 */}
+                            {/* <Route path="register" element={<Register />} /> */}
+                            {/* <Route path="login" element={<Login />} /> */}
+
+                            {/* 채팅 관련 라우트 */}
+                            <Route path="chat">
+                                <Route index element={<ChatList />} />
+                                <Route path=":roomId" element={<ChatRoom />} />
+                            </Route>
+
+                            {/* 404 페이지 처리 */}
+                            <Route path="*" element={<div>Page Not Found</div>} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
             </ModalProvider>
         </AuthProvider>
     );
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,11 +1,10 @@
-// src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'  // App 컴포넌트 import
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />  // RouterProvider 대신 App 컴포넌트 사용
+        <App />
     </React.StrictMode>,
 )
