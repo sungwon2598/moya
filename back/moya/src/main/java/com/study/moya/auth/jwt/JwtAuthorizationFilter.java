2e08d6db94c8c5a4538de60f11eb36119ad60043
hjg727 <tony9800@naver.com>
2024-12-05 21:10:31 +0900

    feat : Swagger 적용

--- back/moya/src/main/java/com/study/moya/auth/jwt/JwtAuthorizationFilter.java
+++ back/moya/src/main/java/com/study/moya/auth/jwt/JwtAuthorizationFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -77,5 +79,24 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         }
         return null;
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        log.info("지금 들어온 요청은 스킵됩니다. URI: {}", request.getRequestURI());
+        String[] excludePath = {"/swagger-ui/index.html",
+                "/swagger-ui/swagger-ui-standalone-preset.js",
+                "/swagger-ui/swagger-initializer.js",
+                "/swagger-ui/swagger-ui-bundle.js",
+                "/swagger-ui/swagger-ui.css",
+                "/swagger-ui/index.css",
+                "/favicon.ico",
+                "/swagger-ui/favicon-32x32.png",
+                "/swagger-ui/favicon-16x16.png",
+                "/api-docs/json/swagger-config",
+                "/api-docs/json"};
+        String path = request.getRequestURI();
+        return Arrays.stream(excludePath).anyMatch(path::startsWith);
+    }
+
 
 }
