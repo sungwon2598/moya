5eee162c407854efe1c6a3e106624cfc5e2ad2c1
dlgkrals <dlgkrals1136@naver.com>
2024-12-06 13:39:03 +0900

    KAN-72 feat: ai 로드맵 엔티티 및 서비스, 컨트롤러 추가

---
+++ back/moya/src/main/java/com/study/moya/ai_roadmap/dto/RoadmapRequest.java
@@ -0,0 +1,45 @@
+package com.study.moya.ai_roadmap.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class RoadmapRequest {
+
+    @NotBlank(message = "Main category is required")
+    private String mainCategory; // 대분류
+
+    @NotBlank(message = "Subcategory is required")
+    private String subCategory; // 중분류
+
+    @NotBlank(message = "Goal level is required")
+    private String goalLevel;
+
+    private String currentLevel;
+
+    @NotNull(message = "Duration is required")
+    @Min(value = 1, message = "Duration must be at least 1 week")
+    private Integer duration; // 주 단위
+
+    // 기본 생성자
+    public RoadmapRequest() {
+    }
+
+    // toString 메소드 오버라이드
+    @Override
+    public String toString() {
+        return "RoadmapRequest{" +
+                "goalLevel='" + goalLevel + '\'' +
+                ", currentLevel='" + currentLevel + '\'' +
+                ", mainCategory='" + mainCategory + '\'' +
+                ", subCategory='" + subCategory + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+}
