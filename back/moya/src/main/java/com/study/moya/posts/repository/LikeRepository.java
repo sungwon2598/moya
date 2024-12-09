cff01b736a5e0adf570f63f67ae3ac311baaa909
hjg727 <tony9800@naver.com>
2024-12-09 14:06:41 +0900

    feat : Service 목업 추가

--- back/moya/src/main/java/com/study/moya/posts/repository/LikeRepository.java
+++ back/moya/src/main/java/com/study/moya/posts/repository/LikeRepository.java
@@ -3,5 +3,8 @@ package com.study.moya.posts.repository;
 import com.study.moya.posts.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LikeRepository extends JpaRepository<Like, Long> {
+    Optional<Like> findByMemberIdAndPostId(Long memberId, Long postId);
 }
