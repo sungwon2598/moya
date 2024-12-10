f2d7120f0bfc0a442ee221715bd13536eafa61b1
Gogogo10102 <{ID}+{username}@users.noreply.github.com>
2024-12-10 14:43:43 +0900

    KAN-100 feat. accesstoken 발급 기능 삭제, json api로 OAuthLoginResponse 전송

--- back/moya/src/main/java/com/study/moya/oauth/controller/LoginController.java
+++ back/moya/src/main/java/com/study/moya/oauth/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.study.moya.oauth.controller;
 
 import com.google.common.net.HttpHeaders;
 import com.study.moya.oauth.dto.IdTokenRequestDto;
+import com.study.moya.oauth.dto.OAuthLogin.MemberAuthResult;
+import com.study.moya.oauth.dto.OAuthLogin.OAuthLoginResponse;
 import com.study.moya.oauth.service.OauthService;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -24,14 +26,35 @@ public class LoginController {
         if (requestBody == null || requestBody.getCredential() == null) {
             throw new IllegalArgumentException("ID Token is required");
         }
-        String authToken = oauthService.loginOAuthGoogle(requestBody);
-        final ResponseCookie cookie = ResponseCookie.from("AUTH-TOKEN", authToken)
+        MemberAuthResult authResult = oauthService.loginOAuthGoogle(requestBody);
+
+        final ResponseCookie cookie = ResponseCookie.from("AUTH-TOKEN", authResult.getToken())
                 .httpOnly(true)
                 .maxAge(7 * 24 * 3600)
                 .path("/")
                 .secure(false)
                 .build();
         response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
-        return ResponseEntity.ok().build();
+
+        OAuthLoginResponse loginResponse = OAuthLoginResponse.from(authResult.getMember());
+        return ResponseEntity.ok(loginResponse);
     }
+
+
+
+//    @PostMapping("/login")
+//    public ResponseEntity<?> LoginWithGoogleOauth2(@RequestBody IdTokenRequestDto requestBody, HttpServletResponse response) {
+//        if (requestBody == null || requestBody.getCredential() == null) {
+//            throw new IllegalArgumentException("ID Token is required");
+//        }
+//        String authToken = oauthService.loginOAuthGoogle(requestBody, requestBody.getRedirectUrl());
+//        final ResponseCookie cookie = ResponseCookie.from("AUTH-TOKEN", authToken)
+//                .httpOnly(true)
+//                .maxAge(7 * 24 * 3600)
+//                .path("/")
+//                .secure(false)
+//                .build();
+//        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+//        return ResponseEntity.ok().build();
+//    }
 }
