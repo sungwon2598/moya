e2a885a32fb66e764bfc6b08ee1bd0fff8989a86
pillow12360 <ehdcks1256@naver.com>
2024-12-22 20:20:50 +0900

    KAN-124 fix: 배포 오류로 재배포

--- front/src/features/auth/store/authSlice.ts
+++ front/src/features/auth/store/authSlice.ts
@@ -12,7 +12,6 @@ const initialState: AuthState = {
     tokens: undefined
 };
 
-// Google login thunk
 export const authenticateWithGoogleThunk = createAsyncThunk<AuthResponseData, GoogleAuthResponse>(
     'auth/authenticateWithGoogle',
     async (authData, { rejectWithValue }) => {
