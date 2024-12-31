ed1a6054aca117f2ec96af7b145ae34a7a5ac479
hjg727 <tony9800@naver.com>
2024-12-31 11:37:08 +0900

    feat : @Repository 추가

--- back/moya/src/main/java/com/study/moya/posts/repository/CommentRepository.java
+++ back/moya/src/main/java/com/study/moya/posts/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package com.study.moya.posts.repository;
 
 import com.study.moya.posts.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
--- back/moya/src/main/java/com/study/moya/posts/repository/LikeRepository.java
+++ back/moya/src/main/java/com/study/moya/posts/repository/LikeRepository.java
@@ -2,9 +2,11 @@ package com.study.moya.posts.repository;
 
 import com.study.moya.posts.domain.Like;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByMemberIdAndPostId(Long memberId, Long postId);
 
