89d9ac935742b928d89ed156ec30462a3b4a0446
kiseon77 <kieson.han77@gmail.com>
2025-03-03 12:42:16 +0900

    KAN-154 hooks

--- front/src/features/auth/hooks/useAdmin.ts
+++ front/src/features/auth/hooks/useAdmin.ts
@@ -1,8 +1,8 @@
-import { useAuth } from './useAuth';
+import { useAuth } from "./useAuth";
 
 export const useAdmin = () => {
-    const { user } = useAuth();
-    return {
-        isAdmin: user?.roles?.includes('ROLE_ADMIN') ?? false
-    };
+  const { user } = useAuth();
+  return {
+    isAdmin: user?.roles?.includes("ADMIN") ?? false,
+  };
 };
