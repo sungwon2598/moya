2cdbb57ccc6524219a40cf12e8524242aa14bc93
kiseon77 <kieson.han77@gmail.com>
2025-03-08 12:02:42 +0900

    fix : index.css 수정

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         moya: {
-          primary: '#0752cd',    // 메인 컬러
-          secondary: '#2a83fc',  // 세컨더리 컬러
-          background: '#f5f5f8', // 서브 컬러
-        }
-      }
+          primary: "#0752cd", // 메인 컬러
+          secondary: "#2a83fc", // 세컨더리 컬러
+          background: "#f5f5f8", // 서브 컬러
+        },
+      },
     },
   },
   plugins: [],
-}
+};
