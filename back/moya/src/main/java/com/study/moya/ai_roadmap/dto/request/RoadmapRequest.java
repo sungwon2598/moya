6ed52d3c8db5017d07b44656da74573b7520b915
dlgkrals <dlgkrals1136@naver.com>
2025-04-04 16:29:19 +0900

    KAN-175 feat: dto에 스웨거에 보여주기 위한 예시 추가

--- back/moya/src/main/java/com/study/moya/ai_roadmap/dto/request/RoadmapRequest.java
+++ back/moya/src/main/java/com/study/moya/ai_roadmap/dto/request/RoadmapRequest.java
@@ -1,6 +1,7 @@
 package com.study.moya.ai_roadmap.dto.request;
 
 import com.study.moya.ai_roadmap.constants.LearningObjective;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,20 +12,26 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Schema(description = "로드맵 생성 요청")
 public class RoadmapRequest {
 
     @NotBlank(message = "Main category is required")
+    @Schema(description = "중분류", example = "프로그래밍 언어")
     private String mainCategory; // 중분류
 
     @NotBlank(message = "Subcategory is required")
+    @Schema(description = "주제", example = "C#")
     private String subCategory; // 주제
 
+    @Schema(description = "현재 수준 : 1(초급) 2(중급) 3(고급)", example = "1")
     private String currentLevel; // 현재 수준 : 1(초급) 2(중급) 3(고급)
 
     @NotNull(message = "Duration is required")
     @Min(value = 1, message = "Duration must be at least 1 week")
+    @Schema(description = "학습 기간(주 단위)", example = "8")
     private Integer duration; // 주 단위
 
+    @Schema(description = "학습 목표", example = "BASIC_UNDERSTANDING")
     private LearningObjective learningObjective; // 학습목표
 
     // 기본 생성자
