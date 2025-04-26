fc2161153a6f5fe36abceb972590509009053bbf
kiseon77 <kieson.han77@gmail.com>
2025-04-26 15:00:34 +0900

    KAN-170 내 로드맵 페이지 퍼블리싱

--- front/src/pages/roadmap/CreatePage.tsx
+++ front/src/pages/roadmap/CreatePage.tsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react";
-import Title from "../../components/roadmap/Title.tsx";
-import Select from "../../components/roadmap/Select/Select";
-import { RoadmapQuestionStageType } from "./type";
-import { useRoadmapQuestions } from "@/features/roadmap/hooks/useRoadmapQuestions";
-import Progress from "@/components/roadmap/Progress";
+import { useEffect, useState } from 'react';
+import Title from '../../components/roadmap/Title.tsx';
+import Select from '../../components/roadmap/Select/Select';
+import { useRoadmapQuestions } from '@/features/roadmap/hooks/useRoadmapQuestions';
+import Progress from '@/components/roadmap/Progress';
+export interface RoadmapQuestionStageType {
+  currentStatusNumber: number;
+}
 
 export type AnswerItem = {
   questionNumber: number | string;
@@ -14,23 +16,19 @@ export type AnswerItem = {
 
 export default function CreatePage() {
   //api요청
-  const { questions, isLoading, error, setSelectedCategoryId } =
-    useRoadmapQuestions();
+  const { questions, isLoading, error, setSelectedCategoryId } = useRoadmapQuestions();
   // 질문ID, 선택ID 저장
   const [answers, setAnswers] = useState<AnswerItem[] | null>([]);
   // 1번 질문부터, 현재 질문페이지 저장
-  const [roadmapQuestionStage, setRoadmapQuestionStage] =
-    useState<RoadmapQuestionStageType>({
-      currentStatusNumber: 1,
-    });
+  const [roadmapQuestionStage, setRoadmapQuestionStage] = useState<RoadmapQuestionStageType>({
+    currentStatusNumber: 1,
+  });
 
-  const categoryQuestion = questions.find((q) => q.name === "mainCategory");
+  const categoryQuestion = questions.find((q) => q.name === 'mainCategory');
 
   useEffect(() => {
     if (categoryQuestion) {
-      const categoryAnswer = answers?.find(
-        (answer) => answer.questionNumber === categoryQuestion.id
-      );
+      const categoryAnswer = answers?.find((answer) => answer.questionNumber === categoryQuestion.id);
       if (categoryAnswer) {
         setSelectedCategoryId(categoryAnswer.choiceId);
       }
@@ -43,10 +41,7 @@ export default function CreatePage() {
   return (
     <div className="@container mx-auto grid grid-cols-2 gap-4 px-4">
       <Title />
-      <Progress
-        TotalQuestionsNumber={questions.length}
-        roadmapQuestionStage={roadmapQuestionStage}
-      />
+      <Progress TotalQuestionsNumber={questions.length} roadmapQuestionStage={roadmapQuestionStage} />
       <Select
         questions={questions}
         answers={answers}
