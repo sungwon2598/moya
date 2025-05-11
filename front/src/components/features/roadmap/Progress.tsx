c153089deb33137596cdebf9540e541282015d9c
nwejin <147528674+nwejin@users.noreply.github.com>
2025-05-11 22:55:13 +0900

    fix roadmap progress

--- front/src/components/features/roadmap/Progress.tsx
+++ front/src/components/features/roadmap/Progress.tsx
@@ -1,17 +1,17 @@
-import { Progress as ShadcnProgress } from "@/components/shared/ui/progress";
-import { RoadmapQuestionStageType } from "@/pages/roadmap/type";
+import { Progress as ShadcnProgress } from '@/components/shared/ui/progress';
+// import { RoadmapQuestionStageType } from '@/pages/roadmap/type';
+
+export interface RoadmapQuestionStageType {
+  currentStatusNumber: number;
+}
 
 interface ProgressProp {
   roadmapQuestionStage: RoadmapQuestionStageType;
   TotalQuestionsNumber: number;
 }
 
-export default function Progress({
-  roadmapQuestionStage,
-  TotalQuestionsNumber,
-}: ProgressProp) {
-  const progressPercentage =
-    (roadmapQuestionStage.currentStatusNumber / TotalQuestionsNumber) * 100;
+export default function Progress({ roadmapQuestionStage, TotalQuestionsNumber }: ProgressProp) {
+  const progressPercentage = (roadmapQuestionStage.currentStatusNumber / TotalQuestionsNumber) * 100;
 
   return (
     <section className="col-span-2 my-6">
