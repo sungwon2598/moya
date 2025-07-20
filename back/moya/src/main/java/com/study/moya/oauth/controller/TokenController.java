ba9b4d8898cad7d6b9465bc999c1a7474df4d7d5
Gogogo10102 <{ID}+{username}@users.noreply.github.com>
2025-07-20 20:26:06 +0900

    hotfix. login redis문제로 임시처리

--- back/moya/src/main/java/com/study/moya/oauth/controller/TokenController.java
+++ back/moya/src/main/java/com/study/moya/oauth/controller/TokenController.java
@@ -1,77 +1,77 @@
-package com.study.moya.oauth.controller;
-
-import com.google.common.net.HttpHeaders;
-import com.study.moya.global.api.ApiResponse;
-import com.study.moya.global.config.security.SecurityHeadersConfig;
-import com.study.moya.oauth.dto.token.TokenRefreshResponse;
-import com.study.moya.oauth.exception.OAuthErrorCode;
-import com.study.moya.oauth.service.TokenService;
-import com.study.moya.swagger.annotation.SwaggerErrorDescription;
-import com.study.moya.swagger.annotation.SwaggerErrorDescriptions;
-import com.study.moya.swagger.annotation.SwaggerSuccessResponse;
-import io.swagger.v3.oas.annotations.Operation;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
-@Controller
-@RequiredArgsConstructor
-@Slf4j
-public class TokenController {
-
-    private final TokenService tokenService;
-    private final SecurityHeadersConfig securityHeadersConfig;
-
-    @Operation(summary = "토큰 갱신", description = "리프레시 토큰을 사용하여 새로운 액세스 토큰과 리프레시 토큰을 발급받습니다.")
-    @SwaggerSuccessResponse(
-            status = 200,
-            name = "토큰이 성공적으로 갱신되었습니다",
-            value = TokenRefreshResponse.class
-    )
-    @SwaggerErrorDescriptions({
-            @SwaggerErrorDescription(
-                    name = "리프레시 토큰 유효하지 않음",
-                    value = OAuthErrorCode.class,
-                    code = "INVALID_REFRESH_TOKEN"
-            ),
-            @SwaggerErrorDescription(
-                    name = "회원 정보를 찾을 수 없음",
-                    value = OAuthErrorCode.class,
-                    code = "MEMBER_NOT_FOUND"
-            )
-    })
-    @PostMapping("/refresh")
-    public ResponseEntity<?> refreshToken(@CookieValue(name = "REFRESH-TOKEN", required = true) String refreshToken,
-                                          HttpServletResponse response) {
-        log.info("refresh 시작-------");
-        TokenRefreshResponse refreshResult = tokenService.refreshToken(refreshToken);
-
-        // 새로운 액세스 토큰을 쿠키에 설정
-        final ResponseCookie accessTokenCookie = ResponseCookie.from("AUTH-TOKEN", refreshResult.getJwtToken())
-                .httpOnly(true)
-                .maxAge(3600) // 1시간
-                .path("/")
-                .secure(false)
-                .build();
-
-        // 새로운 리프레시 토큰을 쿠키에 설정
-        final ResponseCookie refreshTokenCookie = ResponseCookie.from("REFRESH-TOKEN", refreshResult.getRefreshToken())
-                .httpOnly(true)
-                .maxAge(7 * 24 * 3600) // 7일
-                .path("/")
-                .secure(false)
-                .build();
-
-        response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
-        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
-
-        return securityHeadersConfig.addSecurityHeaders(
-                ResponseEntity.ok(ApiResponse.success()));
-    }
-}
+//package com.study.moya.oauth.controller;
+//
+//import com.google.common.net.HttpHeaders;
+//import com.study.moya.global.api.ApiResponse;
+//import com.study.moya.global.config.security.SecurityHeadersConfig;
+//import com.study.moya.oauth.dto.token.TokenRefreshResponse;
+//import com.study.moya.oauth.exception.OAuthErrorCode;
+//import com.study.moya.oauth.service.TokenService;
+//import com.study.moya.swagger.annotation.SwaggerErrorDescription;
+//import com.study.moya.swagger.annotation.SwaggerErrorDescriptions;
+//import com.study.moya.swagger.annotation.SwaggerSuccessResponse;
+//import io.swagger.v3.oas.annotations.Operation;
+//import jakarta.servlet.http.HttpServletResponse;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.ResponseCookie;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CookieValue;
+//import org.springframework.web.bind.annotation.PostMapping;
+//
+//
+//@Controller
+//@RequiredArgsConstructor
+//@Slf4j
+//public class TokenController {
+//
+//    private final TokenService tokenService;
+//    private final SecurityHeadersConfig securityHeadersConfig;
+//
+//    @Operation(summary = "토큰 갱신", description = "리프레시 토큰을 사용하여 새로운 액세스 토큰과 리프레시 토큰을 발급받습니다.")
+//    @SwaggerSuccessResponse(
+//            status = 200,
+//            name = "토큰이 성공적으로 갱신되었습니다",
+//            value = TokenRefreshResponse.class
+//    )
+//    @SwaggerErrorDescriptions({
+//            @SwaggerErrorDescription(
+//                    name = "리프레시 토큰 유효하지 않음",
+//                    value = OAuthErrorCode.class,
+//                    code = "INVALID_REFRESH_TOKEN"
+//            ),
+//            @SwaggerErrorDescription(
+//                    name = "회원 정보를 찾을 수 없음",
+//                    value = OAuthErrorCode.class,
+//                    code = "MEMBER_NOT_FOUND"
+//            )
+//    })
+//    @PostMapping("/refresh")
+//    public ResponseEntity<?> refreshToken(@CookieValue(name = "REFRESH-TOKEN", required = true) String refreshToken,
+//                                          HttpServletResponse response) {
+//        log.info("refresh 시작-------");
+//        TokenRefreshResponse refreshResult = tokenService.refreshToken(refreshToken);
+//
+//        // 새로운 액세스 토큰을 쿠키에 설정
+//        final ResponseCookie accessTokenCookie = ResponseCookie.from("AUTH-TOKEN", refreshResult.getJwtToken())
+//                .httpOnly(true)
+//                .maxAge(3600) // 1시간
+//                .path("/")
+//                .secure(false)
+//                .build();
+//
+//        // 새로운 리프레시 토큰을 쿠키에 설정
+//        final ResponseCookie refreshTokenCookie = ResponseCookie.from("REFRESH-TOKEN", refreshResult.getRefreshToken())
+//                .httpOnly(true)
+//                .maxAge(7 * 24 * 3600) // 7일
+//                .path("/")
+//                .secure(false)
+//                .build();
+//
+//        response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
+//        response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
+//
+//        return securityHeadersConfig.addSecurityHeaders(
+//                ResponseEntity.ok(ApiResponse.success()));
+//    }
+//}
--- back/moya/src/main/java/com/study/moya/oauth/service/OAuthFacadeService.java
+++ back/moya/src/main/java/com/study/moya/oauth/service/OAuthFacadeService.java
@@ -1,83 +1,83 @@
-package com.study.moya.oauth.service;
-
-
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.study.moya.auth.jwt.JwtTokenProvider;
-import com.study.moya.member.domain.Member;
-import com.study.moya.member.repository.MemberRepository;
-import com.study.moya.oauth.dto.auth.OAuthTokenResponse;
-import com.study.moya.oauth.dto.user.GoogleUserInfo;
-import com.study.moya.oauth.dto.auth.OAuthLoginRequest;
-import com.study.moya.oauth.dto.member.MemberAuthResponse;
-import com.study.moya.oauth.exception.OAuthErrorCode;
-import com.study.moya.oauth.exception.OAuthException;
-import com.study.moya.redis.RedisService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.*;
-import java.util.stream.Collectors;
-
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class OAuthFacadeService {
-   private final MemberRepository memberRepository;
-   private final JwtTokenProvider jwtTokenProvider;
-   private final RedisService redisService;
-   private final GoogleOAuthService googleOAuthService;
-   private final MemberOAuthService memberOAuthService;
-
-    /**
-     * OAuth credential 토큰 검증
-     */
-    @Transactional
-    public MemberAuthResponse loginOAuthGoogle(OAuthLoginRequest requestBody) {
-        log.info("Authorization Code: {}", requestBody.getAuthCode());
-        try {
-            OAuthTokenResponse tokenResponse = googleOAuthService.getGoogleTokens(requestBody.getAuthCode(), requestBody.getRedirectUri());
-            GoogleIdToken.Payload idTokenPayload = googleOAuthService.verifyAndGetIdToken(tokenResponse.getIdToken());
-
-            Member savedMember = memberOAuthService.createOrUpdateMember(idTokenPayload, tokenResponse);
-
-            List<GrantedAuthority> authorities = savedMember.getAuthorities().stream()
-                    .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
-                    .collect(Collectors.toList());
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    savedMember,
-                    null,
-                    authorities
-            );
-
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            JwtTokenProvider.TokenInfo tokenInfo = jwtTokenProvider.createToken(authentication);
-            String memberId = String.valueOf(savedMember.getId());
-
-            //임시방책 redis
-//            String existingIdentifier = redisService.findIdentifierByMemberId(memberId);
-
-
-            /**
-             * redis 오류로 인한 임시
-             */
-//            if (existingIdentifier != null) {
-//                redisService.deleteRefreshToken(memberId);
-//            }
-//            redisService.saveTokens(memberId, tokenInfo.getRefreshToken());
-
-            return new MemberAuthResponse(tokenInfo.getAccessToken(), tokenInfo.getRefreshToken(), savedMember);
-        } catch (Exception e){
-            log.error("Failed to process OAuth Google login", e);
-            throw OAuthException.of(OAuthErrorCode.GOOGLE_API_ERROR);
-        }
-    }
-}
+//package com.study.moya.oauth.service;
+//
+//
+//import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+//import com.study.moya.auth.jwt.JwtTokenProvider;
+//import com.study.moya.member.domain.Member;
+//import com.study.moya.member.repository.MemberRepository;
+//import com.study.moya.oauth.dto.auth.OAuthTokenResponse;
+//import com.study.moya.oauth.dto.user.GoogleUserInfo;
+//import com.study.moya.oauth.dto.auth.OAuthLoginRequest;
+//import com.study.moya.oauth.dto.member.MemberAuthResponse;
+//import com.study.moya.oauth.exception.OAuthErrorCode;
+//import com.study.moya.oauth.exception.OAuthException;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.util.*;
+//import java.util.stream.Collectors;
+//
+//
+//@Service
+//@RequiredArgsConstructor
+//@Slf4j
+//public class OAuthFacadeService {
+//   private final MemberRepository memberRepository;
+//   private final JwtTokenProvider jwtTokenProvider;
+//   private final GoogleOAuthService googleOAuthService;
+//   private final MemberOAuthService memberOAuthService;
+//
+//    /**
+//     * OAuth credential 토큰 검증
+//     */
+//    @Transactional
+//    public MemberAuthResponse loginOAuthGoogle(OAuthLoginRequest requestBody) {
+//        log.info("Authorization Code: {}", requestBody.getAuthCode());
+//        try {
+//            OAuthTokenResponse tokenResponse = googleOAuthService.getGoogleTokens(requestBody.getAuthCode(), requestBody.getRedirectUri());
+//            GoogleIdToken.Payload idTokenPayload = googleOAuthService.verifyAndGetIdToken(tokenResponse.getIdToken());
+//
+//            Member savedMember = memberOAuthService.createOrUpdateMember(idTokenPayload, tokenResponse);
+//
+//            List<GrantedAuthority> authorities = savedMember.getAuthorities().stream()
+//                    .map(authority -> new SimpleGrantedAuthority(authority.getAuthority()))
+//                    .collect(Collectors.toList());
+//
+//            Authentication authentication = new UsernamePasswordAuthenticationToken(
+//                    savedMember,
+//                    null,
+//                    authorities
+//            );
+//
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//            JwtTokenProvider.TokenInfo tokenInfo = jwtTokenProvider.createToken(authentication);
+//            String memberId = String.valueOf(savedMember.getId());
+//
+//            savedMember.updateJwtRefreshToken(tokenInfo.getRefreshToken());
+//            memberRepository.save(savedMember);
+//            //임시방책 redis
+////            String existingIdentifier = redisService.findIdentifierByMemberId(memberId);
+//
+//
+//            /**
+//             * redis 오류로 인한 임시
+//             */
+////            if (existingIdentifier != null) {
+////                redisService.deleteRefreshToken(memberId);
+////            }
+////            redisService.saveTokens(memberId, tokenInfo.getRefreshToken())
+//
+//            return new MemberAuthResponse(tokenInfo.getAccessToken(), tokenInfo.getRefreshToken(), savedMember);
+//        } catch (Exception e){
+//            log.error("Failed to process OAuth Google login", e);
+//            throw OAuthException.of(OAuthErrorCode.GOOGLE_API_ERROR);
+//        }
+//    }
+//}
