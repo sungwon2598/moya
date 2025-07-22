118e62903b76a18a2fa51a06ed1aafbf7eeed73f
nwejin <147528674+nwejin@users.noreply.github.co>
2025-07-23 00:35:03 +0900

    KAN-199 update: 스토어정리

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,8 +8,8 @@ import Main from './pages/main/MainPage.tsx';
 // import LearningRoad from '@pages/learningRoad/LearningRoad.tsx';
 
 import { ProfilePage } from './pages/profile/index.tsx';
-import { store } from './store/store.ts';
-import { Provider } from 'react-redux';
+// import { store } from './store/store.ts';
+// import { Provider } from 'react-redux';
 import SignInPage from './pages/auth/SignInPage.tsx';
 import SignUpPage from './pages/auth/SignUpPage.tsx';
 import StudyList from './pages/study/list/index.tsx';
@@ -70,79 +70,79 @@ const App: React.FC = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <ModalProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<RootLayout />}>
-                <Route index element={<Main />} />
-                <Route path="notifications" element={<Notifications />} />
-                <Route path="signin" element={<SignInPage />} />
-                <Route path="signup" element={<SignUpPage />} />
-                {/* 내정보 라우트 */}
-                <Route path="my-info">
-                  <Route index element={<MyPage />} />
-                  <Route path="roadmap" element={<MyRoadmap />} />
-                  <Route path="roadmap/:roadmapId" element={<RoadmapDetail />} />
-                </Route>
-                {/* 로드맵 관련 라우트 */}
-                <Route path="roadmap">
-                  <Route path="create" element={<CreatePage />} />
-                  <Route path="pending" element={<RoadmapPending />} />
-                  <Route path="weeklyPlan" element={<WeeklyPlan />} />
+      {/* <Provider store={store}> */}
+      <ModalProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<RootLayout />}>
+              <Route index element={<Main />} />
+              <Route path="notifications" element={<Notifications />} />
+              <Route path="signin" element={<SignInPage />} />
+              <Route path="signup" element={<SignUpPage />} />
+              {/* 내정보 라우트 */}
+              <Route path="my-info">
+                <Route index element={<MyPage />} />
+                <Route path="roadmap" element={<MyRoadmap />} />
+                <Route path="roadmap/:roadmapId" element={<RoadmapDetail />} />
+              </Route>
+              {/* 로드맵 관련 라우트 */}
+              <Route path="roadmap">
+                <Route path="create" element={<CreatePage />} />
+                <Route path="pending" element={<RoadmapPending />} />
+                <Route path="weeklyPlan" element={<WeeklyPlan />} />
 
-                  {/* <Route path="preview" element={<RoadmapPreview />} />
+                {/* <Route path="preview" element={<RoadmapPreview />} />
                   <Route path="road" element={<LearningRoad />} /> */}
-                </Route>
-
-                {/* 스터디 관련 라우트 */}
-                <Route path="study">
-                  <Route index element={<StudyList />} />
-                  <Route path=":postId" element={<StudyPostDetail />} />
-                  <Route path="create" element={<StudyCreate />} />
-                  <Route path=":postId/edit" element={<StudyEdit />} />
-                </Route>
-
-                <Route
-                  path="go"
-                  element={
-                    <ProtectedRoute>
-                      <AdminRoute>
-                        <Outlet />
-                      </AdminRoute>
-                    </ProtectedRoute>
-                  }>
-                  <Route path="categorys" element={<CategoryManagement />} />
-                  <Route path="create-sample" element={<CreateSample />} />
-                </Route>
-                <Route path="settings/profile" element={<ProfilePage />} />
-
-                {/* 404 페이지 처리 */}
-                <Route
-                  path="*"
-                  element={
-                    <div className="flex min-h-screen items-center justify-center">
-                      <h1 className="text-2xl font-bold text-gray-800">페이지를 찾을 수 없습니다</h1>
-                    </div>
-                  }
-                />
               </Route>
 
-              <Route path="ggo" element={<AdminLayout />}>
-                <Route path="categories" element={<CategoryManagement />} />
-                <Route
-                  path="*"
-                  element={
-                    <div className="flex min-h-screen items-center justify-center">
-                      <h1 className="text-2xl font-bold text-gray-800">관리자 페이지를 찾을 수 없습니다</h1>
-                    </div>
-                  }
-                />
+              {/* 스터디 관련 라우트 */}
+              <Route path="study">
+                <Route index element={<StudyList />} />
+                <Route path=":postId" element={<StudyPostDetail />} />
+                <Route path="create" element={<StudyCreate />} />
+                <Route path=":postId/edit" element={<StudyEdit />} />
               </Route>
-            </Routes>
-          </BrowserRouter>
-        </ModalProvider>
-      </Provider>
+
+              <Route
+                path="go"
+                element={
+                  <ProtectedRoute>
+                    <AdminRoute>
+                      <Outlet />
+                    </AdminRoute>
+                  </ProtectedRoute>
+                }>
+                <Route path="categorys" element={<CategoryManagement />} />
+                <Route path="create-sample" element={<CreateSample />} />
+              </Route>
+              <Route path="settings/profile" element={<ProfilePage />} />
+
+              {/* 404 페이지 처리 */}
+              <Route
+                path="*"
+                element={
+                  <div className="flex min-h-screen items-center justify-center">
+                    <h1 className="text-2xl font-bold text-gray-800">페이지를 찾을 수 없습니다</h1>
+                  </div>
+                }
+              />
+            </Route>
+
+            <Route path="ggo" element={<AdminLayout />}>
+              <Route path="categories" element={<CategoryManagement />} />
+              <Route
+                path="*"
+                element={
+                  <div className="flex min-h-screen items-center justify-center">
+                    <h1 className="text-2xl font-bold text-gray-800">관리자 페이지를 찾을 수 없습니다</h1>
+                  </div>
+                }
+              />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ModalProvider>
+      {/* </Provider> */}
     </QueryClientProvider>
   );
 };
